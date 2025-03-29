50c54e4de6623e0ececa76ae4ec3bdf283709a8b
Ito Jumpei <123927296+itojum@users.noreply.github.com>
2025-03-29 07:00:49 +0000

    add: implement flash messages for user creation success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功を扱う
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
   end
 
   private
-  
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
