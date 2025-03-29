4bc2fc859bb77e8135704b93cb2a910dcc6ab7fb
Ito Jumpei <123927296+itojum@users.noreply.github.com>
2025-03-29 08:35:56 +0000

    add: implement create and destroy actions in sessions controller for user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にリダイレクト
+    else
+      flash[:danger] = "Invalid email/password combination"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    
+  end
 end
