484d46d4fb68d2f8624f1238a3961a61f9f382d4
Ito Jumpei <123927296+itojum@users.noreply.github.com>
2025-03-31 12:28:12 +0000

    test: マイクロポストモデルのテストを追加
    - fixtures/microposts.ymlを削除
    - micropost_test.rbにセットアップメソッドを追加し、マイクロポストの有効性をテスト
    - user_idが存在することを確認するテストを追加

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    @microposts = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @microposts.valid?
+  end
+
+  test "user id should be present" do
+    @microposts.user_id = nil
+    assert_not @microposts.valid?
+  end
 end
