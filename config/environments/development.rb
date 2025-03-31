e81dce9792e2e5aa73e853d4e68663adfc8dff05
Ito Jumpei <123927296+itojum@users.noreply.github.com>
2025-03-31 08:29:56 +0000

    feat: 開発環境のホストURLを更新
    - config/environments/development.rbのホストURLを変更しました。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  host = "reimagined-space-guide-x6q6j7956w9hpwgj.github.dev/"
+  host = "reimagined-space-guide-x6q6j7956w9hpwgj-3000.app.github.dev/"
   config.action_mailer.default_url_options = { host: host, protocol: "https" }
 
   config.action_mailer.perform_caching = false
