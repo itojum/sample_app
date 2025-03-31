c7107580e8a95d4b28f4638baf41dd1bd6a53104
Ito Jumpei <123927296+itojum@users.noreply.github.com>
2025-03-31 06:46:55 +0000

    feat: アカウント有効化メールの内容を更新
    - ApplicationMailerのデフォルト送信者を変更
    - UserMailerのアカウント有効化メソッドを修正し、ユーザー情報を使用
    - アカウント有効化メールのHTMLテンプレートを更新
    - アカウント有効化メールのテキストテンプレートを更新
    - 開発環境のメール設定を追加
    - UserMailerのプレビューを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = "reimagined-space-guide-x6q6j7956w9hpwgj.github.dev/"
+  config.action_mailer.default_url_options = { host: host, protocol: "https" }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
