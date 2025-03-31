a4c842faa9e58f7a972eb550094b1ac1540355a8
Ito Jumpei <123927296+itojum@users.noreply.github.com>
2025-03-31 13:31:52 +0000

    feat: マイクロポストモデルにデフォルトスコープを追加し、ユーザーモデルに関連付けを追加
    - マイクロポストモデルにデフォルトスコープを追加し、作成日時で降順にソート
    - ユーザーモデルにマイクロポストとの関連付けを追加し、削除時に依存するマイクロポストも削除
    - マイクロポストのテストを修正し、ユーザーとの関連を考慮
    - マイクロポストのフィクスチャを追加

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,25 +3,25 @@ require "test_helper"
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @microposts = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
-    assert @microposts.valid?
+    assert @micropost.valid?
   end
 
   test "user id should be present" do
-    @microposts.user_id = nil
-    assert_not @microposts.valid?
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
   end
 
   test "content should be present" do
-    @microposts.content = "   "
-    assert_not @microposts.valid?
+    @micropost.content = "   "
+    assert_not @micropost.valid?
   end
 
   test "content should be at most 140 characters" do
-    @microposts.content = "a" * 141
-    assert_not @microposts.valid?
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
   end
 end
